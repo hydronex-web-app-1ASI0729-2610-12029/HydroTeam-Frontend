4a9e67a0ff8a77b0894ce0b8ead59ca64aa184ad
Bruno <brunorodolfomontalvan@gmail.com>
2026-05-14 09:32:08 -0500

    feat(iam): add user entity

---
+++ src/app/iam/domain/user.entity.ts
@@ -0,0 +1,58 @@
+// src/app/iam/domain/user.entity.ts
+
+export enum UserRole {
+  ADMINISTRATOR = 'ADMINISTRATOR',
+  RESIDENT = 'RESIDENT',
+}
+
+export interface UserProps {
+  userId: string;
+  name: string;
+  email: string;
+  password?: string;
+  role: UserRole;
+  phoneNumber?: string;
+  apartmentNumber?: string;
+}
+
+export class User {
+  readonly userId: string;
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+  readonly role: UserRole;
+
+  constructor(props: UserProps) {
+    this.userId = props.userId;
+    this.name = props.name;
+    this.email = props.email;
+    this.password = props.password ?? '';
+    this.role = props.role;
+  }
+
+  get isAdministrator(): boolean {
+    return this.role === UserRole.ADMINISTRATOR;
+  }
+
+  get isResident(): boolean {
+    return this.role === UserRole.RESIDENT;
+  }
+}
+
+export class Administrator extends User {
+  readonly phoneNumber: string;
+
+  constructor(props: UserProps & { phoneNumber: string }) {
+    super({ ...props, role: UserRole.ADMINISTRATOR });
+    this.phoneNumber = props.phoneNumber;
+  }
+}
+
+export class Resident extends User {
+  readonly apartmentNumber: string;
+
+  constructor(props: UserProps & { apartmentNumber: string }) {
+    super({ ...props, role: UserRole.RESIDENT });
+    this.apartmentNumber = props.apartmentNumber;
+  }
+}
